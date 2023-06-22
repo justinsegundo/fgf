47cad28e45e780bceb36f44bbece6b7d759a8e2c
justinsegundo <134711597+justinsegundo@users.noreply.github.com>
2023-06-22 07:33:45 -0700

    changes

--- fgf/settings.py
+++ fgf/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'fgf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'astronaut/Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
